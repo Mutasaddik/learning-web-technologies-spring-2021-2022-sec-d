08bf8bed211cc80bbe358e3b521c4b56f9fc9737
Mutasaddik <98294847+Mutasaddik@users.noreply.github.com>
2022-02-09 09:58:16 +0600

    Add files via upload

---
+++ php/Task.php
@@ -0,0 +1,18 @@
+<?php
+function rect_area($length , $width ) 
+{
+    $area = 2*$length * $width;
+    echo "Area Of Rectangle  " . $area ;
+}
+rect_area(4,5);
+
+
+ function calculate($products,$vat/100){
+
+
+ 	$Total_amount=$products*$vat;
+ 	echo "Total Amount: ".$Total_amount;
+ }
+
+calculate (500,10);
+?>
